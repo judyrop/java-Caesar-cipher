4bf32a1aece7beb438b0758d8ee13bb872f8fdc2
Your Name <youremail@domain.com>
2021-09-05 01:16:45 +0300

    created private modifier to shit test in decryption class

--- src/main/java/Decryption.java
+++ src/main/java/Decryption.java
@@ -1,9 +1,14 @@
 public class Decryption {
     private String cipherText;
+    private int shift;
 public Decryption(String cipherText,int shift){
 this.cipherText = cipherText;
+this.shift = shift;
 }
     public  String getCipherText() {
         return this.cipherText;
+    }
+    public int getShift(){
+    return this.shift;
     }
 }
