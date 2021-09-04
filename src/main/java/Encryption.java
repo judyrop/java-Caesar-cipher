2c4f32268ec6b141d2330913f64b9f46dcfc61b6
Your Name <youremail@domain.com>
2021-09-04 23:26:46 +0300

    created private modifier to complete the plaintext test

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -2,9 +2,10 @@
 public class Encryption {
     private String plainText;
     public Encryption(String plainText, int shift){
+        this.plainText = plainText;
     }
 
     public  String getPlainText() {
-      return ;
+      return this.plainText;
     }
 }
